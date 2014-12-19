9f4fc2af90a433b56cb8fa3a6f0c9aa91476bc18
Masashi Terui <masashi.terui@jig-saw.com>
2014-12-19 19:05:07 +0900

    first commit

---
+++ kitchen-docker_cli.gemspec
@@ -0,0 +1,29 @@
+# coding: utf-8
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+require 'kitchen/driver/docker_cli_version'
+
+Gem::Specification.new do |spec|
+  spec.name          = 'kitchen-docker_cli'
+  spec.version       = Kitchen::Driver::DOCKER_CLI_VERSION
+  spec.authors       = ['Masashi Terui']
+  spec.email         = ['marcy9114@gmail.com']
+  spec.description   = %q{A Test Kitchen Driver for Docker native CLI}
+  spec.summary       = spec.description
+  spec.homepage      = 'https://github.com/marcy-terui/kitchen-docker_cli'
+  spec.license       = 'Apache 2.0'
+
+  spec.files         = `git ls-files`.split($/)
+  spec.executables   = []
+  spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
+  spec.require_paths = ['lib']
+
+  spec.add_dependency 'test-kitchen', github: 'test-kitchen/test-kitchen', ref: '237efd1'
+
+  spec.add_development_dependency 'bundler', '~> 1.3'
+  spec.add_development_dependency 'rake'
+
+  spec.add_development_dependency 'cane'
+  spec.add_development_dependency 'tailor'
+  spec.add_development_dependency 'countloc'
+end
---
+++ lib/kitchen/driver/docker_cli.rb
@@ -0,0 +1,65 @@
+# -*- encoding: utf-8 -*-
+#
+# Author:: Masashi Terui (<marcy9114@gmail.com>)
+#
+# Copyright (C) 2014, Masashi Terui
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'kitchen'
+
+module Kitchen
+
+  module Driver
+
+    # Docker CLI driver for Kitchen.
+    #
+    # @author Masashi Terui <marcy9114@gmail.com>
+    class DockerCLI < Kitchen::Driver::Base
+
+      def create(state)
+        state[:image] = docker_build unless state[:image]
+        state[:container_id] = docker_run(state) unless state[:container_id]
+      end
+
+      protected
+
+      def docker_build
+        cmd = "build"
+        cmd << " --no-cache" unless config[:no_cache]
+        output = docker_command(cmd, :input => docker_file)
+        container_id = output.chomp
+        unless container_id.match(/^[0-9a-z]+$/)
+          raise ActionFailed, "Build failed. Could not set CONTAINER ID."
+        end
+        container_id
+      end
+
+      def docker_run(state)
+      end
+
+      def docker_file
+        file = ["FROM #{config[:image]}\n"]
+        Array(config[:run_command]).each {|cmd| file << "RUN #{cmd}\n"}
+        file.join('\n')
+      end
+
+      def docker_command(cmd, opts={})
+        cmd = "docker #{cmd}"
+        run_command(cmd, opts)
+      end
+
+
+    end
+  end
+end
