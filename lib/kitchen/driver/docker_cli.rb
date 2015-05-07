e7595ae690647630fbdfcadd3ad38c7f05abd742
Masashi Terui <masashi.terui@jig-saw.com>
2015-05-07 14:12:58 +0900

    support test-kitchen 1.4.0 #7

--- lib/kitchen/driver/docker_cli.rb
+++ lib/kitchen/driver/docker_cli.rb
@@ -27,8 +27,6 @@ module Kitchen
     # @author Masashi Terui <marcy9114@gmail.com>
     class DockerCli < Kitchen::Driver::Base
 
-      include ShellOut
-
       default_config :no_cache, false
       default_config :command, 'sh -c \'while true; do sleep 1d; done;\''
       default_config :privileged, false
@@ -56,70 +54,30 @@ module Kitchen
       end
 
       def create(state)
-        state[:image] = build unless state[:image]
-        state[:container_id] = run(state[:image]) unless state[:container_id]
-      end
-
-      def converge(state)
-        provisioner = instance.provisioner
-        provisioner.create_sandbox
-
-        execute(docker_exec_command(state[:container_id], provisioner.install_command, :tty => true)) if provisioner.install_command
-        execute(docker_exec_command(state[:container_id], provisioner.init_command, :tty => true)) if provisioner.init_command
-        execute(docker_pre_transfer_command(provisioner, state[:container_id]))
-        run_command(docker_transfer_command(provisioner, state[:container_id]))
-        execute(docker_exec_command(state[:container_id], provisioner.prepare_command, :tty => true)) if provisioner.prepare_command
-        execute(docker_exec_command(state[:container_id], provisioner.run_command, :tty => true)) if provisioner.run_command
-
-      ensure
-        provisioner && provisioner.cleanup_sandbox
-      end
-
-      def setup(state)
-        if busser.setup_cmd
-          execute(docker_exec_command(state[:container_id], busser.setup_cmd, :tty => true))
-        end
-      end
-
-      def verify(state)
-        if busser.sync_cmd
-          execute(docker_exec_command(state[:container_id], busser.sync_cmd, :tty => true))
-        end
-        if busser.run_cmd
-          execute(docker_exec_command(state[:container_id], busser.run_cmd, :tty => true))
-        end
+        state[:image] = build(state) unless state[:image]
+        state[:container_id] = run(state) unless state[:container_id]
       end
 
       def destroy(state)
-        execute("rm -f #{state[:container_id]}") rescue false
+        instance.transport.connection(state) do |conn|
+          conn.run_docker("rm -f #{state[:container_id]}") rescue false
+        end
       end
 
-      def remote_command(state, cmd)
-        execute(docker_exec_command(state[:container_id], cmd))
-      end
-
-      def login_command(state)
-        args = []
-        args << 'exec'
-        args << '-t'
-        args << '-i'
-        args << state[:container_id]
-        args << '/bin/bash'
-        LoginCommand.new('docker', args)
-      end
-
-      def build
-        output = execute(docker_build_command, :input => docker_file)
+      def build(state)
+        output = ""
+        instance.transport.connection(state) do |conn|
+          output = conn.run_docker(docker_build_command, :input => docker_file)
+        end
         parse_image_id(output)
       end
 
-      def run(image)
-        output = execute(docker_run_command(image))
+      def run(state)
+        output = ""
+        instance.transport.connection(state) do |conn|
+          output = conn.run_docker(docker_run_command(state[:image]))
+        end
         parse_container_id(output)
-      end
-
-      def docker_command(cmd)
-        "docker #{cmd}"
       end
 
       def docker_build_command
@@ -145,26 +103,6 @@ module Kitchen
         Array(config[:volume]).each { |vol| cmd << " -v #{vol}" }
         Array(config[:link]).each { |link| cmd << " --link #{link}" }
         cmd << " #{image} #{config[:command]}"
-      end
-
-      def docker_exec_command(container_id, cmd, opt = {})
-        exec_cmd = "exec"
-        exec_cmd << " -t" if opt[:tty]
-        exec_cmd << " -i" if opt[:interactive]
-        # exec_cmd << " <<-EOH\n#{cmd}\nEOH"
-        exec_cmd << " #{container_id} #{cmd}"
-      end
-
-      def docker_pre_transfer_command(provisioner, container_id)
-        cmd = "mkdir -p #{provisioner[:root_path]}"
-        cmd << " && rm -rf #{provisioner[:root_path]}/*"
-        docker_exec_command(container_id, cmd)
-      end
-
-      def docker_transfer_command(provisioner, container_id)
-        remote_cmd = "tar x -C #{provisioner[:root_path]}"
-        local_cmd  = "cd #{provisioner.sandbox_path} && tar cf - ./"
-        "#{local_cmd} | #{docker_command(docker_exec_command(container_id, remote_cmd, :interactive => true))}"
       end
 
       def parse_image_id(output)
@@ -200,10 +138,6 @@ module Kitchen
           Array(config[:environment]).each { |env, value| file << "ENV #{env}=#{value}" }
           file.join("\n")
         end
-      end
-
-      def execute(cmd, opts = {})
-        run_command(docker_command(cmd), opts)
       end
 
 
--- lib/kitchen/transport/docker_cli.rb
+++ lib/kitchen/transport/docker_cli.rb
@@ -1,9 +1,86 @@
+# -*- encoding: utf-8 -*-
+#
+# Author:: Masashi Terui (<marcy9114@gmail.com>)
+#
+# Copyright (C) 2015, Masashi Terui
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 require "kitchen"
+require 'thor/util'
 
 module Kitchen
   module Transport
     class DockerCli < Kitchen::Transport::Base
-      class Connection < Kitchen::Transport::Base::Connection ; end
+
+      class DockerCliFailed < TransportFailed; end
+
+      kitchen_transport_api_version 1
+      plugin_version Kitchen::VERSION
+
+      default_config :host, nil
+
+      def connection(state, &block)
+        options = config.to_hash.merge(state)
+        @connection = Kitchen::Transport::DockerCli::Connection.new(options, &block)
+      end
+
+      class Connection < Kitchen::Transport::DockerCli::Connection
+
+        include ShellOut
+
+        def login_command
+          args = []
+          args << 'exec'
+          args << '-t'
+          args << '-i'
+          args << @options[:container_id]
+          args << '/bin/bash'
+          LoginCommand.new(binary, args)
+        end
+
+        def execute(command)
+          exec_cmd = docker_exec_command(@options[:container_id], command, :tty => true)
+          run_docker(exec_cmd) if command
+        end
+
+        def run_docker(command, options={})
+          run_command("#{binary} #{command}", options)
+        end
+
+        def upload(locals, remote)
+          cmd = "mkdir -p #{remote} && rm -rf #{remote}/*"
+          execute(cmd)
+          Array(locals).each do |local|
+            remote_cmd = "tar x -C #{remote}"
+            remote_cmd = "#{binary} #{docker_exec_command(@options[:container_id], remote_cmd, :interactive => true)}"
+            local_cmd  = "cd #{File.dirname(local)} && tar cf - ./#{File.basename(local)}"
+            run_command("#{local_cmd} | #{remote_cmd}")
+          end
+        end
+
+        def docker_exec_command(container_id, cmd, opt = {})
+          exec_cmd = "exec"
+          exec_cmd << " -t" if opt[:tty]
+          exec_cmd << " -i" if opt[:interactive]
+          exec_cmd << " #{container_id} #{cmd}"
+        end
+
+        def binary
+          "docker"
+        end
+
+      end
     end
   end
 end
